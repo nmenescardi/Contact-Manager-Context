177e4a95d894e649885e21e4366e846739697863
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-04-16 19:55:00 -0400

    Updates

---
+++ precache-manifest.b3f422245f04fbbb5d26a4a173884f25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "840feaba9dd513701630",
+    "url": "/Contact-Manager-Provider/static/js/runtime~main.2775087e.js"
+  },
+  {
+    "revision": "060a5937fd09e5f65a78",
+    "url": "/Contact-Manager-Provider/static/js/main.7899f6b1.chunk.js"
+  },
+  {
+    "revision": "28425666d5fd7fb1987e",
+    "url": "/Contact-Manager-Provider/static/js/2.776a6499.chunk.js"
+  },
+  {
+    "revision": "060a5937fd09e5f65a78",
+    "url": "/Contact-Manager-Provider/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "28425666d5fd7fb1987e",
+    "url": "/Contact-Manager-Provider/static/css/2.af10606f.chunk.css"
+  },
+  {
+    "revision": "8bbe6b7d02f835ad7129d4bdd8617806",
+    "url": "/Contact-Manager-Provider/index.html"
+  }
+];
