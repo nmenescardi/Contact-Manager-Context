9168d230e6026791f1f04d77d175e01a493b11ec
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-04-16 05:17:12 -0400

    Add stuff to Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 
 export default class Contact extends Component {
   render() {
-    return <div />;
+    return (
+      <div className="">
+        <h4>Jonh</h4>
+        <ul>
+          <li>Email: algodon@hotmail.com</li>
+          <li>Phone: 555-555-5555</li>
+        </ul>
+      </div>
+    );
   }
 }
