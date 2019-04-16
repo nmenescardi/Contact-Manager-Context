3d7c75f64696aed708cc6fb8fcc88e5f96e19d58
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-04-16 07:13:35 -0400

    Add fontawesome and icon

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class Contact extends Component {
+  state = {};
+  onShowClick = () => {
+    console.log(this.state);
+  };
   render() {
     const { name, email, phone } = this.props;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
+        <h4>
+          {name} <i onClick={this.onShowClick} className="fas fa-sort-down" />
+        </h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item">Phone: {phone}</li>
