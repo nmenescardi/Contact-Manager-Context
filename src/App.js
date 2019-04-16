d30877669d11705b6f544447d8345dd82541df56
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-04-16 06:02:22 -0400

    Add bootstrap and general layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import './App.css';
 import Contact from './components/Contact';
 import Header from './components/Header';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Contact name="Carlos" email="algodon@gmail.com" phone="3052216632" />
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact
+            name="Carlos Smith"
+            email="algodon@gmail.com"
+            phone="3052216632"
+          />
+          <Contact
+            name="Karen Busca Rueda"
+            email="kbr@gmail.com"
+            phone="3052216632"
+          />
+        </div>
       </div>
     );
   }
