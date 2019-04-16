bc691f3bb987a889de8f96041efd17f9cfe86fdc
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-04-16 09:46:24 -0400

    add controlled component and onChange event to imputs

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -6,13 +6,24 @@ export default class AddContact extends Component {
     email: '',
     phone: ''
   };
+
+  onChange = e =>
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state, e);
+  };
+
   render() {
     const { name, email, phone } = this.state;
     return (
       <div className="card mb-3">
         <div className="card-header">Add Contact</div>
         <div className="card-body">
-          <form>
+          <form onSubmit={this.onSubmit}>
             <div className="form-group">
               <label htmlFor="name">Name</label>
               <input
@@ -22,6 +33,7 @@ export default class AddContact extends Component {
                 name="name"
                 id="name"
                 value={name}
+                onChange={this.onChange}
               />
             </div>
             <div className="form-group">
@@ -33,6 +45,7 @@ export default class AddContact extends Component {
                 name="email"
                 id="email"
                 value={email}
+                onChange={this.onChange}
               />
             </div>
             <div className="form-group">
@@ -44,6 +57,7 @@ export default class AddContact extends Component {
                 name="phone"
                 id="phone"
                 value={phone}
+                onChange={this.onChange}
               />
             </div>
             <input
